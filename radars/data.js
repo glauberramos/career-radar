4c601b435cfa196c4dcbf1efe17d02c28cb9d40f
Glauber Ramos <glauberamos@gmail.com>
2012-03-15 16:29:49 -0300

    Changing data

--- radars/data.js
+++ radars/data.js
@@ -1,33 +1,39 @@
-title = 'Your Name, Month Year';
+title = 'Glauber Ramos, February 2012';
 
 data = {
-    'First Quadrant': {
-        'Very High Skill': 4,
-        'High Skill': 3,
-        'Medium Skill': 2,
-        'Low Skill': 1,
+    'Languages': {
+        'HTML/CSS': 4,
+        'Javascript': 3,
+        '.NET/C#': 3,
+        'Java': 3,
+        'LESS, SASS and SCSS': 4,
+        'Ruby': 1,
     },
 
-    'Second Quadrant': {
-        'Very High Skill': 4,
-        'High Skill': 3,
-        'Medium Skill': 2,
-        'Low Skill': 1,
+    'Tools': {
+        'GIT': 4,
+        'GITHUB': 3,
+        'VIM': 3,
+        'Visual Studio': 4,
+        'Eclipe': 2,
+        'Intelij': 3,
+        'Resharper': 2,
     },
 
-    'Third Quadrant': {
-        'Very High Skill': 4,
-        'High Skill': 3,
-        'Medium Skill': 2,
-        'Low Skill': 1,
+    'Techniques': {
+        'Pairing': 4,
+        'TDD': 3,
+        'Refactoring': 3,
+        'BDD': 2,
     },
 
-    'Fourth Quadrant': {
-        'Very High Skill': 4,
-        'High Skill': 3,
-        'Medium Skill': 2,
-        'Low Skill': 1,
+    'Design': {
+        'Markup Prototypes': 3,
+        'User Experience': 3,
+        'Mockups': 3,
+        'Illustrator': 3,
+        'Photoshop': 3,
+        'Lightroom': 3,
+        'InDesign and Corel': 2,
     },
-
 };
-
